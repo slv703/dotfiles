587e83a278498778fce94b84b326e2f7b22732b0
Alexander Markov <aleksandr.b.markov@gmail.com>
2024-02-02 09:23:01 +0300

    Added hotkeys

--- nvim/lua/base/mappings.lua
+++ nvim/lua/base/mappings.lua
@@ -2,8 +2,17 @@
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
 -- Change without yanking
 vim.keymap.set("n", "c", '"_c')
 vim.keymap.set("v", "c", '"_c')
 vim.keymap.set("n", "C", '"_C')
 vim.keymap.set("v", "C", '"_C')
+
+-- Сбросить визуальное выделение поисковой фразы
+vim.keymap.set("n", "<leader>vr", "<Cmd>noh<CR>")
+
+vim.keymap.set("n", "<leader>mm", "<Cmd>!mattermost-desktop<CR><CR>")
+
+vim.keymap.set("n", "<leader>iri", "<Cmd>!docker-compose restart insales<CR><CR>")
+vim.keymap.set("n", "<leader>irs", "<Cmd>!docker-compose restart sidekiq<CR><CR>")
