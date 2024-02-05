d2125f331a182c38ea5783b658471596939ee204
Alexander Markov <aleksandr.b.markov@gmail.com>
2024-02-05 17:37:56 +0300

    Add pronto hotkey

--- nvim/lua/base/mappings.lua
+++ nvim/lua/base/mappings.lua
@@ -14,5 +14,9 @@ vim.keymap.set("n", "<leader>vr", "<Cmd>noh<CR>")
 
 vim.keymap.set("n", "<leader>mm", "<Cmd>!mattermost-desktop<CR><CR>")
 
+-- inSales
+
+vim.keymap.set("n", "<leader>ip", "<Cmd>vsplit term://docker-compose exec insales bundler exec pronto run<CR>")
+
 vim.keymap.set("n", "<leader>iri", "<Cmd>!docker-compose restart insales<CR><CR>")
 vim.keymap.set("n", "<leader>irs", "<Cmd>!docker-compose restart sidekiq<CR><CR>")
