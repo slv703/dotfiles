3a80c1ea1cf8cbcf9445f9bcd01a64af01d82335
Alexander Markov <aleksandr.b.markov@gmail.com>
2024-02-19 10:00:38 +0300

    inSales URL mappings

--- nvim/lua/base/mappings.lua
+++ nvim/lua/base/mappings.lua
@@ -15,8 +15,14 @@ vim.keymap.set("n", "<leader>vr", "<Cmd>noh<CR>")
 vim.keymap.set("n", "<leader>mm", "<Cmd>!mattermost-desktop<CR><CR>")
 
 -- inSales
-
+vim.keymap.set("n", "<leader>bro", "<Cmd>!xdg-open $INSALES_BRO770_LOGIN_URL<CR><CR>")
+vim.keymap.set("n", "<leader>idm", "<Cmd>!xdg-open $DAILY_MEETING_URL<CR><CR>")
+vim.keymap.set("n", "<leader>ifs", "<Cmd>!xdg-open $INSALES_GITLAB_STOPPED_STAGINGS_URL<CR><CR>")
+vim.keymap.set("n", "<leader>imp", "<Cmd>!xdg-open $INSALES_GITLAB_MASTER_PIPLINES_URL<CR><CR>")
+vim.keymap.set("n", "<leader>imr", "<Cmd>!xdg-open $INSALES_GITLAB_MY_OPEN_MERGE_REQUESTS_URL<CR><CR>")
 vim.keymap.set("n", "<leader>ip", "<Cmd>vsplit term://docker-compose exec insales bundler exec pronto run<CR>")
-
 vim.keymap.set("n", "<leader>iri", "<Cmd>!docker-compose restart insales<CR><CR>")
 vim.keymap.set("n", "<leader>irs", "<Cmd>!docker-compose restart sidekiq<CR><CR>")
+
+-- git
+vim.keymap.set("n", "<leader>gdm", "<Cmd>vsplit term://git diff master...<CR>")
