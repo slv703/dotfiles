9cd031e4fb257f7d26921b8458166a4cad3d0e3c
Aleksandr Markov <aleksandr.markov@insales.ru>
2025-04-18 14:33:32 +0300

    Disable mouse, add langmap

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,8 @@ require('base/tabs')
 require('commands/format')
 require('commands/other')
 require("config.lazy")
+
+-- disable mouse
+vim.o.mouse = ""
+
+vim.opt.langmap = "ФИСВУАПРШОЛДЬТЩЗЙКЫЕГМЦЧНЯ;ABCDEFGHIJKLMNOPQRSTUVWXYZ,фисвуапршолдьтщзйкыегмцчня;abcdefghijklmnopqrstuvwxyz"
