9ad15688d2d20a2500fdaa5097d7950ba672697e
Aleksandr Markov <aleksandr.markov@insales.ru>
2025-06-02 19:48:05 +0300

    Add keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -38,5 +38,41 @@ vim.keymap.set("n", "<leader>sdm", "<Cmd>!gitStashApplyByName ngrok_domain<CR><C
 -- git
 vim.keymap.set("n", "<leader>gdm", "<Cmd>vsplit term://git diff master...<CR>")
 
+-- tabs
+vim.keymap.set("n", "<leader>tc", "<Cmd>tabclose<CR>")
+
+-- windows (en)
+vim.keymap.set("n", "<M-j>", "<C-w>j")
+vim.keymap.set("n", "<M-k>", "<C-w>k")
+vim.keymap.set("n", "<M-h>", "<C-w>h")
+vim.keymap.set("n", "<M-l>", "<C-w>l")
+
+-- windows (ru)
+vim.keymap.set("n", "<M-о>", "<C-w>j")
+vim.keymap.set("n", "<M-л>", "<C-w>k")
+vim.keymap.set("n", "<M-р>", "<C-w>h")
+vim.keymap.set("n", "<M-д>", "<C-w>l")
+
+vim.keymap.set("n", "<leader>wr", "<C-w>r")
+vim.keymap.set("n", "<leader>we", "<C-w>R")
+
+vim.keymap.set("n", "<leader>wk", "<C-w>K")
+vim.keymap.set("n", "<leader>wj", "<C-w>J")
+vim.keymap.set("n", "<leader>wh", "<C-w>H")
+vim.keymap.set("n", "<leader>wl", "<C-w>L")
+
+vim.keymap.set("n", "<leader>wx", "<C-w>x")
+vim.keymap.set("n", "<leader>wt", "<C-w>T")
+
+vim.keymap.set("i", "<M-c>", "<C-S-c>")
+vim.keymap.set("i", "<M-v>", "<C-S-v>")
+
+-- Выход из терминального режима по клавише Esc
+vim.keymap.set('t', '<Esc>', '<C-\\><C-n>')
+vim.keymap.set("n", "<Esc><Esc>", "<C-w>c")
+
+vim.keymap.set("v", "<", "<gv")
+vim.keymap.set("v", ">", ">gv")
+
 -- inoremap <silent><expr> <TAB> pumvisible() ? "\<C-n>" : "\<TAB>"
 -- inoremap <expr><S-TAB> pumvisible() ? "\<C-p>" : "\<C-h>"
